3111c9b6ace4040e9a6ecf831fca8c12086e3ea8
leohlim <leohlim@gmail.com>
2022-08-14 19:32:32 -0500

    Basic operations and logic pseudocode

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// A calculator script that can add, subtract, multiply and divide and show its operations/results on a screen
+
+
+// Basic calculator functions
+
+function add(a, b) {
+    return a + b;
+    }
+    
+function subtract(a, b) {
+    return a - b;
+    }
+    
+function multiply(a, b) {
+    return a * b;
+    }
+    
+function divide(a, b) { 
+    return a / b;
+    }
+
+
+// Operate function that takes two numbers and an operator, and returns the result
+    
+function operate (a, b, operator) {
+    // If operator equals the given operator 
+    // Return that operation passing in a and b 
+}
+
+    
+    
+    
+    
+function updateDisplay() { 
+    // Display zero by default
+    // If user inputs a number or a string 
+    
+    // If an operator after a number 
+    // Or a number after an operator 
+    
+    // Clear the display
+    // Evaluate the equation if possible 
+    // Update it to show new number 
+    
+    // If the number is long, round it 
+    
+    // Make sure equals only works with all operators/ numbers added
+    
+    // If user tries to divide by zero, send them an error message
+    }
+    
+function clear() {
+    // Wipe the current memory
+    // Wipe display and reset to zero  
+}
+    
+    
