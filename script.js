78eef0183ec0209d92262a00a82d91d88727c206
leohlim <leohlim@gmail.com>
2022-08-15 01:03:36 -0500

    Updated CSS, began work on calculator, attained basic UI and operand functionality

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 
 // A calculator script that can add, subtract, multiply and divide and show its operations/results on a screen
 
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation) {
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand;
+    }
+}
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
@@ -10,8 +47,21 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
 
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
 
 
 // Operate function that takes two numbers and an operator, and returns the result
@@ -31,31 +81,5 @@ function operate(a, b, operator) {
     }
 }
 
-    
-    
-    
-    
-function updateDisplay() { 
-    // Display zero by default
-    // If user inputs a number or a string 
-    
-    // If an operator after a number 
-    // Or a number after an operator 
-    
-    // Clear the display
-    // Evaluate the equation if possible 
-    // Update it to show new number 
-    
-    // If the number is long, round it 
-    
-    // Make sure equals only works with all operators/ numbers added
-    
-    // If user tries to divide by zero, send them an error message
-    }
-    
-function clear() {
-    // Wipe the current memory
-    // Wipe display and reset to zero  
-}
     
     
